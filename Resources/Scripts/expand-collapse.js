5ba56a5220fe84c59dcea924ce35130078d1e311
Zvi Gilbert <Zvi.Gilbert@gmail.com>
2026-06-14 12:32:44 -0400

    rejigging to actual CP files from git

---
+++ Resources/Scripts/expand-collapse.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function() {
+		var ls = localStorage.getItem('collapsed');
+		if (ls == "yes"){
+			$(".sidenav-wrapper").hide();
+			$(".collapse").addClass("expand");
+			} 
+		if ($(".expand").length){
+			localStorage.setItem('collapsed', "yes");
+		} else {
+			localStorage.setItem('collapsed', "no");
+		}
+	$(".collapse").click(function() {
+		$(".sidenav-wrapper").toggle(350);
+		$(this).toggleClass("expand");
+		var val = $(this).hasClass('expand') ? 'yes' : 'no';	
+		localStorage.setItem('collapsed', val);
+	});
+});
---
+++ Resources/Scripts/resize-initializer.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function() {
+	$(".sidenav-wrapper").resizable({
+		minWidth: 290
+	});
+	$(".ui-resizable-e").prop("title", "Resize navigation");
+});
